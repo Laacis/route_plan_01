061b356ab25337aee7f0cb0111fca9ff02d603d6
Laacis <j.lacis.code@gmail.com>
2022-07-26 12:51:15 +0100

    i don't like this solution
     I wish I could write a recursive method returning int[] instead of void, and passing the ref int[] feels wrong, no idea why but I wish I could find another way

--- PathFinderTask.cs
+++ PathFinderTask.cs
@@ -1,23 +1,45 @@
 using System;
-using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 
 namespace RoutePlanning
 {
-	public static class PathFinderTask
-	{
-		public static int[] FindBestCheckpointsOrder(Point[] checkpoints)
-		{
-			var bestOrder = MakeTrivialPermutation(checkpoints.Length);
-			return bestOrder;
-		}
+    public static class PathFinderTask
+    {
+        public static int[] FindBestCheckpointsOrder(Point[] checkpoints)
+        {
+            var minValue = new double[] { int.MaxValue };
+            var result = new int[checkpoints.Length];
+            MakePermutations(new int[checkpoints.Length], 1, ref result, checkpoints, minValue);
+            return result;
+        }
 
-		private static int[] MakeTrivialPermutation(int size)
-		{
-			var bestOrder = new int[size];
-			for (int i = 0; i < bestOrder.Length; i++)
-				bestOrder[i] = i;
-			return bestOrder;
-		}
-	}
+        static void MakePermutations(int[] permutation, int position, ref int[] result, Point[] checkpoints, double[] minValue)
+        {
+            if (position == permutation.Length)
+            {
+                minValue[0] = PointExtensions.GetPathLength(checkpoints, permutation);
+                result = (int[])permutation.Clone();
+                return; 
+            }
+            else
+            {
+                for (int i = 1; i < permutation.Length; i++)
+                {
+                    var index = Array.IndexOf(permutation, i, 0, position);
+                    if (index != -1)
+                    {
+                        continue;
+                    }
+                    permutation[position] = i;
+                    if (PointExtensions.GetPathLength(checkpoints,
+                                                      permutation.Take(position + 1).ToArray()) >= minValue[0])
+                    {
+                        continue;
+                    }
+                    MakePermutations(permutation, position + 1, ref result, checkpoints, minValue);
+                }
+            }
+        }
+    }
 }
